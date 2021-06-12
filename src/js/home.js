c2b575c83bf95caa2ec7b954cdbc5f4604376ec9
Yuri Felipe <yurirflp@gmail.com>
2021-06-12 12:40:15 -0300

    Atualizando modal e tela da pagina home

--- src/js/home.js
+++ src/js/home.js
@@ -2,7 +2,7 @@
 let myModal = new bootstrap.Modal(document.getElementById('myModal'));
 
 //Criando referência com o botão "INICIE AGORA"
-let btn = document.querySelector('.index-buttonContainer');
+let btn = document.querySelector('.index-button');
 
 //Atribuindo evento click ao botão
 btn.addEventListener('click', toggleModal);
